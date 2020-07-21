5b8fb3fba68e289f0544a6e42735ad6e5b852eef
san99tiago <san99tiago@gmail.com>
2020-07-20 22:57:05 -0500

    Completed unit-test for solution of problem 2 LeetCode

--- leet_code/problem_2/test_add_two_numbers_1.py
+++ leet_code/problem_2/test_add_two_numbers_1.py
@@ -6,7 +6,6 @@ from add_two_numbers_1 import ListNode, Solution
 
 
 class TestAddTwoNumbers(unittest.TestCase):
-
     def setUp(self):
         # Create all ListNode objects for the tests
         self.l1 = ListNode(2, ListNode(4, ListNode(3)))
@@ -27,9 +26,38 @@ class TestAddTwoNumbers(unittest.TestCase):
         self.assertEqual(self.sol.get_full_number(self.l4), 0)
         self.assertEqual(self.sol.get_full_number(self.l5), 999)
 
+    def test_result_list(self):
+        result_list_A = self.sol.result_list("243")
+        self.assertEqual(self.sol.get_full_number(result_list_A), 342)
+
+        result_list_B = self.sol.result_list("564")
+        self.assertEqual(self.sol.get_full_number(result_list_B), 465)
+
+        result_list_C = self.sol.result_list("52")
+        self.assertEqual(self.sol.get_full_number(result_list_C), 25)
+
+        result_list_D = self.sol.result_list("0")
+        self.assertEqual(self.sol.get_full_number(result_list_D), 0)
+
+        result_list_E = self.sol.result_list("999")
+        self.assertEqual(self.sol.get_full_number(result_list_E), 999)
+
     def test_add_two_numbers(self):
-        result_list_A = self.sol.addTwoNumbers(l1, l2)
-        self.assertEqual(result_list_A, )
+        result_list_A = self.sol.addTwoNumbers(self.l1, self.l2)
+        self.assertEqual(self.sol.get_full_number(result_list_A), 807)
+
+        result_list_B = self.sol.addTwoNumbers(self.l1, self.l3)
+        self.assertEqual(self.sol.get_full_number(result_list_B), 367)
+
+        result_list_C = self.sol.addTwoNumbers(self.l2, self.l4)
+        self.assertEqual(self.sol.get_full_number(result_list_C), 465)
+
+        result_list_D = self.sol.addTwoNumbers(self.l3, self.l5)
+        self.assertEqual(self.sol.get_full_number(result_list_D), 1024)
+
+        result_list_E = self.sol.addTwoNumbers(self.l1, self.l5)
+        self.assertEqual(self.sol.get_full_number(result_list_E), 1341)
+
 
 if __name__ == "__main__":
     unittest.main()
